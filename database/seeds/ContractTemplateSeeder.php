ff35318233b2a66ea6995e54bf1d265603dc4eb9
artem <temicska99@mail.ru>
2020-02-16 19:39:32 +0200

    ContractTemplateSeeder

--- database/seeds/ContractTemplateSeeder.php
+++ database/seeds/ContractTemplateSeeder.php
@@ -2,10 +2,30 @@
 
 use App\Models\Contract\ContractTemplate;
 
+/**
+ * Class ContractTemplateSeeder
+ *
+ * Наполнитель для образцов договоров
+ */
 class ContractTemplateSeeder extends MyDataBaseSeeder
 {
     public function run()
     {
         $this->fillModel(ContractTemplate::class, 'data_contract_template');
+
+        if (env('ENV_TYPE') == 'dev') {
+            $this->randomData(35);
+        }
+    }
+
+    private function randomData($count): void
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $template = new ContractTemplate();
+            $template->name = $this->getFaker()->name;
+            $template->contract_title = $template->name;
+
+            $template->save();
+        }
     }
 }
