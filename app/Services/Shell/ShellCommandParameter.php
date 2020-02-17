4eff001b0402207518333415ea5da4cbab35de37
artem <temicska99@mail.ru>
2020-02-17 23:32:52 +0200

    ShellCommandParameter add quotes

--- app/Services/Shell/ShellCommandParameter.php
+++ app/Services/Shell/ShellCommandParameter.php
@@ -7,13 +7,18 @@ class ShellCommandParameter implements ShellSettingInterface
     /** @var string */
     private $string;
 
+    /** @var bool */
+    private $quotes = false;
+
     /**
      * ShellCommandParameter constructor.
      * @param string $string
+     * @param bool $quotes
      */
-    public function __construct(string $string)
+    public function __construct(string $string, bool $quotes = false)
     {
         $this->string = $string;
+        $this->quotes = $quotes;
     }
 
     /**
@@ -21,6 +26,6 @@ class ShellCommandParameter implements ShellSettingInterface
      */
     public function getString(): string
     {
-        return $this->string;
+        return $this->quotes ? ('"' . $this->string . '"') : $this->string;
     }
 }
