5ff20846daec08b1a73996b7daf250885dd33193
ArtARTs36 <temicska99@mail.ru>
2020-03-31 16:44:34 +0300

    add permissions

--- app/Models/User/Permission.php
+++ app/Models/User/Permission.php
@@ -44,6 +44,24 @@ final class Permission extends BasePermission
 
     const USER_GET_NOTIFICATION_USER_REGISTERED = 'user_get_notification_user_registered';
 
+    const CONTRACTS_LIST_VIEW = 'contracts_list_view';
+    const CONTRACTS_VIEW = 'contracts_view';
+    const CONTRACTS_CREATE = 'contracts_create';
+    const CONTRACTS_EDIT = 'contracts_edit';
+    const CONTRACTS_DELETE = 'contracts_delete';
+
+    const PRODUCTS_LIST_VIEW = 'products_list_view';
+    const PRODUCTS_VIEW = 'products_view';
+    const PRODUCTS_CREATE = 'products_create';
+    const PRODUCTS_EDIT = 'products_edit';
+    const PRODUCTS_DELETE = 'products_delete';
+
+    const EXTERNAL_NEWS_LIST_VIEW = 'external_news_list_view';
+    const EXTERNAL_NEWS_VIEW = 'external_news_view';
+    const EXTERNAL_NEWS_CREATE = 'external_news_create';
+    const EXTERNAL_NEWS_EDIT = 'external_news_edit';
+    const EXTERNAL_NEWS_DELETE = 'external_news_delete';
+
     public static function getAllNames()
     {
         return [
@@ -74,6 +92,24 @@ final class Permission extends BasePermission
             self::VOCAB_BANKS_LIST_VIEW => 'Просмотр списка банков',
 
             self::USER_GET_NOTIFICATION_USER_REGISTERED => 'Получать уведомления о регистрациях пользователей',
+
+            self::CONTRACTS_LIST_VIEW => 'Просмотр списка договоров',
+            self::CONTRACTS_VIEW => 'Просмотр договора',
+            self::CONTRACTS_CREATE => 'Создание договора',
+            self::CONTRACTS_EDIT => 'Редактирование договора',
+            self::CONTRACTS_DELETE => 'Удаление договора',
+
+            self::PRODUCTS_LIST_VIEW => 'Просмотр списка товаров',
+            self::PRODUCTS_VIEW => 'Просмотр товара',
+            self::PRODUCTS_CREATE => 'Создание товара',
+            self::PRODUCTS_EDIT => 'Редактирование товара',
+            self::PRODUCTS_DELETE => 'Удаление товара',
+
+            self::EXTERNAL_NEWS_LIST_VIEW => 'Просмотр списка новостей из внешнего источников',
+            self::EXTERNAL_NEWS_VIEW => 'Просмотр новостей из внешнего источникаа',
+            self::EXTERNAL_NEWS_CREATE => 'Создание новостей из внешнего источникаа',
+            self::EXTERNAL_NEWS_EDIT => 'Редактирование новостей из внешнего источникаа',
+            self::EXTERNAL_NEWS_DELETE => 'Удаление новостей из внешнего источникаа',
         ];
     }
 
