d081dab157f71fa747e838e2e8fdef01cb7d3eb9
ArtARTs36 <temicska99@mail.ru>
2020-04-09 14:19:15 +0300

    update info.md

--- tests/Feature/VariableDefinitionTest.php
+++ tests/Feature/VariableDefinitionTest.php
@@ -14,7 +14,7 @@ class VariableDefinitionTest extends BaseTestCase
     public function testGetModel(): void
     {
         /** @var VariableDefinition $definition */
-        $definition = VariableDefinition::where('model_type_id', 0)->first();
+        $definition = VariableDefinition::where('model_type_id', '>', 0)->first();
 
         self::assertInstanceOf(Model::class, $definition->getModel());
     }
