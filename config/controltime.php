468b5706da11710a28dfa635b0ec657ae720ce60
ArtARTs36 <temicska99@mail.ru>
2020-05-15 00:38:51 +0300

    update

--- config/controltime.php
+++ config/controltime.php
@@ -2,9 +2,15 @@
 
 return [
     'employee' => [
-        'model' => ''
+        'model_class' => '',
+        'table' => 'controltime_employee',
     ],
     'work_condition' => [
-        'model' => \Dba\ControlTime\Models\WorkCondition::class,
+        'model_class' => \Dba\ControlTime\Models\WorkCondition::class,
+    ],
+    'time' => [
+        'model_class' => \Dba\ControlTime\Models\Time::class,
+        'table' => 'controltime_times',
+        'date_format' => 'Y-m-d',
     ],
 ];
