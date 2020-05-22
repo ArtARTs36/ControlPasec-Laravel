3c641fffddba355957a9f412107cb3763186483c
ArtARTs36 <temicska99@mail.ru>
2020-05-22 04:22:55 +0300

    update

--- app/Http/Resource/PermissionResource.php
+++ app/Http/Resource/PermissionResource.php
@@ -12,7 +12,7 @@ use Illuminate\Http\Resources\Json\JsonResource;
  */
 class PermissionResource extends JsonResource
 {
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
             'id' => $this->id,
