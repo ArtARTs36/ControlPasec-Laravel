6667ac6e61a7cabead2e11803d8d25502a5e93b2
ArtARTs36 <temicska99@mail.ru>
2020-06-06 17:34:32 +0300

    update command

--- docker-supervisor-update.sh
+++ docker-supervisor-update.sh
@@ -3,5 +3,7 @@
 # sh docker-supervisor-update.sh
 
 echo "
-\cp -R ControlPasec-Laravel/docker-files/php-fpm/supervisor-apps/* /etc/supervisor/conf.d
+\cp -R /ControlPasec-Laravel/docker-files/php-fpm/supervisor-apps/* /etc/supervisor/conf.d
+supervisorctl update
+supervisorctl start all
 " | docker exec -i Laravel-php-fpm bash
