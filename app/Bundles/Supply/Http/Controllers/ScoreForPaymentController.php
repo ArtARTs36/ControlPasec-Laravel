0b099eced5e7d336573618dee67fb25fbed132b1
ArtARTs36 <temicska99@mail.ru>
2020-12-04 05:38:31 +0300

    move score controller

--- app/Bundles/Supply/Http/Controllers/ScoreForPaymentController.php
+++ app/Bundles/Supply/Http/Controllers/ScoreForPaymentController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Supply;
+namespace App\Bundles\Supply\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Bundles\Supply\Http\Requests\ManySuppliesRequest;
--- app/Bundles/Supply/Http/Routes/api.php
+++ app/Bundles/Supply/Http/Routes/api.php
@@ -10,3 +10,16 @@ Route::prefix('supplies')->group(function () {
 });
 
 Route::apiResource('supplies', 'SupplyController');
+
+// API Для счетов на оплату
+
+Route::prefix('score-for-payments')->group(function () {
+    Route::get('page-{page}', 'ScoreForPaymentController@index');
+});
+
+Route::apiResource('score-for-payments', 'ScoreForPaymentController');
+
+Route::post(
+    'score-for-payments/check-document-of-many',
+    'ScoreForPaymentController@checkOrCreateDocumentOfManyScores'
+);
