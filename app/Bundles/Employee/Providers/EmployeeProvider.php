4806d4a468d3ceac3cf816d9729c704730ff72e6
ArtARTs36 <temicska99@mail.ru>
2020-12-10 07:35:17 +0300

    move employee factory into bundle

--- app/Bundles/Employee/Providers/EmployeeProvider.php
+++ app/Bundles/Employee/Providers/EmployeeProvider.php
@@ -6,8 +6,12 @@ use App\Based\Contracts\BundleProvider;
 
 final class EmployeeProvider extends BundleProvider
 {
+    protected $factoriesPath = __DIR__ . '/../Database/Factories/';
+
     public function register()
     {
         $this->app->register(RouteProvider::class);
+
+        $this->registerFactories();
     }
 }
