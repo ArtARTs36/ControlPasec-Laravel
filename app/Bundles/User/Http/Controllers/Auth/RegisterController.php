d45f6004a6bba0bb7233d7803ce73b84ab33ae80
ArtARTs36 <temicska99@mail.ru>
2020-12-10 05:32:48 +0300

    move UserRegisteredResponse

--- app/Bundles/User/Http/Controllers/Auth/RegisterController.php
+++ app/Bundles/User/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Bundles\User\Http\Controllers\Auth;
 use App\Bundles\User\Events\UserRegistered;
 use App\Http\Controllers\Controller;
 use App\Bundles\User\Http\Requests\UserRegisterRequest;
-use App\Http\Responses\UserRegisteredResponse;
+use App\Bundles\User\Http\Responses\UserRegisteredResponse;
 use App\Bundles\User\Models\Role;
 use App\Bundles\User\Repositories\UserRepository;
 use App\User;
