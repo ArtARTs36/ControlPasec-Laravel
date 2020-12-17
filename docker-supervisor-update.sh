5443691dade0abe380b088dbd0e75fdedfe8443f
ArtARTs36 <temicska99@mail.ru>
2020-12-17 03:11:53 +0300

    cron

--- docker-supervisor-update.sh
+++ docker-supervisor-update.sh
@@ -6,5 +6,4 @@ echo "
 yes|/bin/cp -R -f  /ControlPasec-Laravel/docker-files/php-fpm/supervisor-apps/* /etc/supervisor/conf.d
 supervisorctl update
 supervisorctl restart horizon:*
-supervisorctl restart cron_listen:*
 " | docker exec -i Laravel-php-fpm bash
