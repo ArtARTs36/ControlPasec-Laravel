6f1426a49d355fa1cdffe1afcdb35599f68f04d4
ArtARTs36 <temicska99@mail.ru>
2021-02-18 01:33:20 +0300

    clean command

--- app/Based/Console/Commands/CompileFontFromDompdfCommand.php
+++ app/Based/Console/Commands/CompileFontFromDompdfCommand.php
@@ -9,33 +9,11 @@ use Illuminate\Console\Command;
 
 class CompileFontFromDompdfCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'compile-font-from-dompdf';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
 
     /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
-    /**
-     * Execute the console command.
-     *
      * @throws \Exception
      */
     public function handle()
