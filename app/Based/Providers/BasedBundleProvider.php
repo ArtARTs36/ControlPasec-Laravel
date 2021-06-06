3809f1ffc2d6357e31d66de54342c3ea1c49e2b6
ArtARTs36 <temicska99@mail.ru>
2021-06-07 01:53:06 +0300

    add command

--- app/Based/Providers/BasedBundleProvider.php
+++ app/Based/Providers/BasedBundleProvider.php
@@ -3,12 +3,14 @@
 namespace App\Based\Providers;
 
 use App\Based\Contracts\BundleProvider;
+use ArtARTs36\LaravelBlockIp\Console\Commands\GetNewIps;
 use ArtARTs36\LaravelHoliday\Console\FetchHolidays;
 
 class BasedBundleProvider extends BundleProvider
 {
     protected $commands = [
         FetchHolidays::class,
+        GetNewIps::class,
     ];
 
     public function register()
