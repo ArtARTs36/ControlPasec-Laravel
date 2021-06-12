b9ca667f389bb2935812c94e20f84bd8f5f9ddf6
ArtARTs36 <temicska99@mail.ru>
2021-06-12 17:26:33 +0300

    update system snapshot

--- app/Bundles/Admin/Http/Controllers/SystemController.php
+++ app/Bundles/Admin/Http/Controllers/SystemController.php
@@ -6,6 +6,7 @@ use App\Based\Contracts\Controller;
 use App\Bundles\Admin\Http\Resources\SavedSnapshotResource;
 use App\Bundles\Admin\Http\Resources\SnapshotResource;
 use App\Bundles\Admin\Models\SystemSnapshot;
+use App\Bundles\Admin\Repositories\SystemSnapshotRepository;
 use ArtARTs36\SystemInfo\Contracts\System;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Collection;
@@ -14,9 +15,12 @@ class SystemController extends Controller
 {
     protected $system;
 
-    public function __construct(System $system)
+    protected $snapshots;
+
+    public function __construct(System $system, SystemSnapshotRepository $snapshots)
     {
         $this->system = $system;
+        $this->snapshots = $snapshots;
     }
 
     /**
@@ -24,7 +28,7 @@ class SystemController extends Controller
      */
     public function snapshots(): AnonymousResourceCollection
     {
-        return SavedSnapshotResource::collection(SystemSnapshot::all());
+        return SavedSnapshotResource::collection($this->snapshots->all());
     }
 
     /**
--- app/Bundles/Admin/Repositories/SystemSnapshotRepository.php
+++ app/Bundles/Admin/Repositories/SystemSnapshotRepository.php
@@ -3,8 +3,13 @@
 namespace App\Bundles\Admin\Repositories;
 
 use App\Based\Contracts\Repository;
+use App\Bundles\Admin\Models\SystemSnapshot;
+use Illuminate\Support\Collection;
 
 class SystemSnapshotRepository extends Repository
 {
-    //
+    public function all(): Collection
+    {
+        return $this->newQuery()->orderByDesc(SystemSnapshot::CREATED_AT)->get();
+    }
 }
